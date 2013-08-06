0f37b0e1b46787c7283ec188376375c0b2712032
alrusdi <alrusdi@gmail.com>
2013-08-06 16:04:39 +0600

    Some code incapsulation

--- Scripts/keyboard.js
+++ Scripts/keyboard.js
@@ -1,28 +1,25 @@
-window.addEventListener('keyup', function(event) { Key.onKeyup(event); }, false);
-window.addEventListener('keydown', function(event) { Key.onKeydown(event); }, false);
-
 var Key = {
-  _pressed: {},
+	_pressed: {},
 
-  A: 65,
-  W: 87,
-  D: 68,
-  S: 83,
-  P: 80,
-  SPACE: 32,
-  
-  
-  isDown: function(keyCode) {
-    return this._pressed[keyCode];
-  },
-  
-  onKeydown: function(event) {
-    this._pressed[event.keyCode] = true;
-  },
-  
-  onKeyup: function(event) {
-    delete this._pressed[event.keyCode];
-  }
+	A: 65,
+	W: 87,
+	D: 68,
+	S: 83,
+	P: 80,
+	SPACE: 32,
+
+
+	isDown: function(keyCode) {
+		return Key._pressed[keyCode];
+	},
+
+	onKeydown: function(event) {
+		Key._pressed[event.which] = true;
+	},
+
+	onKeyup: function(event) {
+		delete Key._pressed[event.which];
+	}
 };
 
 
